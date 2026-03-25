8c75f939767cd8da018df441e8c35d6451a1e11f
xonidu <xonidu@gmail.com>
2026-03-25 10:11:59 -0600

    Programa

--- start.py
+++ start.py
@@ -5,7 +5,7 @@
 XONIMET 2026 - Lanzador Universal de Extractor de Metadatos
 Este script ejecuta xonimet.py y verifica las dependencias
 Desarrollado por: Darian Alberto Camacho Salas
-SOMOS XONIDU
+SOMOS: XONIDU
 """
 
 import subprocess
